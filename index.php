fa7aaea35086273ab47684a81f9416605587bc6f
Jeffrey Hartmann <jephmann@gmail.com>
2011-08-05 15:47:34 -0500

    dropdown for itt accounts via csv; if/then for gmail domains

--- index.php
+++ index.php
@@ -3,60 +3,102 @@
     include_once('scripts/class.phpmailer.php');
     include('scripts/class.smtp.php'); // optional, gets called from within class.phpmailer.php if not already loaded
     
+    $optionSELECT = '<option value="">Please SELECT</option>';
+    $optionALL = '<option value="*">ALL</option>';
+    $optionNONE = '<option value="">* AWAITING DATA *</option>';
+    $dosubmit=('<input type="submit" name="submittedForm" value="SEND EMAILS" />');
+    $nosubmit=('<p>This form cannot submit without mailing list data.</p>');
+    
+    $dataitm=('data/itm.csv');
+    $datastudents=('data/students.csv');
+    
+    $optionfrom=$optionSELECT;
+    $openitm=fopen($dataitm,'r');
+    while(!feof($openitm)){
+        $itmrecord=fgetcsv($openitm,1024);
+        $itmaccount=$itmrecord[0];
+        $optionfrom.=('<option value="'.$itmaccount.'">'.$itmaccount.'</option>');
+    }
+    fclose($openitm);    
+    
     $feedback=('');
-    $optionstatus=('');
+    $optionto=('');
     $submit=('');
     $rawstatus=array();
-    $nosubmit=('<p>This form cannot submit without mailing list data.</p>');
-    $csv=('data/students.csv');    
-    if(file_exists($csv)){    
-        if(filesize($csv) != 0){
-            $submit=('<input type="submit" name="submittedForm" value="SEND EMAILS" />');
+    
+    if(file_exists($datastudents)){    
+        if(filesize($datastudents) != 0){
+            $submit=$dosubmit;
+            
             // set up status dropdown
-            $optionstatus='<option value="">Please SELECT</option>';
-            $csv=fopen($csv, 'r');
-            while(!feof($csv)){
-                $csvrecord=fgetcsv($csv,1024);
-                $userstatus=$csvrecord[2];
-                if ($userstatus != null || $userstatus != '' || strlen(trim($userstatus)) != 0){
-                    $rawstatus[]=$userstatus;
+            $optionto=$optionSELECT;
+            $openstudents=fopen($datastudents, 'r');
+            while(!feof($openstudents)){
+                $studentrecord=fgetcsv($openstudents,1024);
+                $studentstatus=$studentrecord[2];
+                if ($studentstatus != null || $studentstatus != '' || strlen(trim($studentstatus)) != 0){
+                    $rawstatus[]=$studentstatus;
                 }
             }
-            fclose($csv);
+            fclose($openstudents);
             $uniquestatus=array_unique($rawstatus);
             foreach ($uniquestatus as $status){
-                $optionstatus.='<option value="'.$status.'">'.$status.'</option>';
+                $optionto.='<option value="'.$status.'">'.$status.'</option>';
             }
-            $optionstatus.='<option value="*">ALL</option>';
+            $optionto.=$optionALL;
+            
             // on submit, send emails            
             if (isset($_POST['submittedForm'])) {
-               $csv=('data/students.csv');
-               $studentstatus=$_POST['studentstatus'];
-               $accountname=$_POST['accountname'];
-               $accountemail=$_POST['accountemail'];       
-               $esubject=$_POST['emailsubject'];
-               $ebody=$_POST['emailbody'];
+               // $csv=('data/students.csv');
+               $post_from=$_POST['optionfrom'];
+               $post_to=$_POST['optionto'];
+               $post_subject=$_POST['emailsubject'];
+               $post_body=$_POST['emailbody'];
                // open csv and loop through it
-               $csv = fopen($csv, 'r');
-               while (!feof($csv)) {
-                   $csvrecord = fgetcsv($csv, 1024);
-                   $useremail=$csvrecord[0];     // REQUIRED; if blank, skip to next record
-                   if ($useremail != null || $useremail != '' || strlen(trim($useremail)) != 0){
-                       $userfullname=$csvrecord[1];  // might not use at all if lastname,firstname -- unless...?
-                       $userstatus=$csvrecord[2];    // keyed to a dropdown selection on the form
-                       if ($studentstatus==$userstatus || $studentstatus == '*'){  
+               $openstudents = fopen($datastudents, 'r');
+               while (!feof($openstudents)) {
+                   $studentrecord = fgetcsv($openstudents, 1024);
+                   $studentemail=$studentrecord[0];     // REQUIRED; if blank, skip to next record
+                   if ($studentemail != null || $studentemail != '' || strlen(trim($studentemail)) != 0){
+                       $studentfullname=$studentrecord[1];  // might not use at all if lastname,firstname -- unless...?
+                       $studentstatus=$studentrecord[2];    // keyed to a dropdown selection on the form
+                       if ($post_to == $studentstatus || $post_to == '*'){
+                           
+                            $openitm=fopen($dataitm,'r');
+                            while(!feof($openitm)){
+                                $itmrecord=fgetcsv($openitm,1024);
+                                if($post_from == $itmrecord[0]){
+                                    $itmaccount=$itmrecord[0];
+                                    $itmusernm=$itmrecord[1];
+                                    $itmdomain=$itmrecord[2];
+                                    $itmpsswrd=$itmrecord[3];
+                                }
+                            }
+                            fclose($openitm);
+                            $itmemail=($itmusernm.'@'.$itmdomain);
                             $mail = new PHPMailer();
-                            $mail->IsSMTP();               // set mailer to use SMTP
-                            $mail->Host = ('mail.iit.edu');  // specify main and backup server
-                            $mail->From = $accountemail;
-                            $mail->FromName = $accountname;
-                            $mail->AddAddress($useremail, $userfullname);
-                            $mail->AddReplyTo($accountemail, $accountname);
+                            $mail->IsSMTP();
+                            switch($itmdomain){
+                                case 'gmail.com':
+                                    $mail->Host = ('smtp.'.$itmdomain);
+                                    $mail->SMTPAuth = true;
+                                    $mail->SMTPSecure = "ssl";
+                                    $mail->Port = 465;
+                                    $mail->Username = $itmemail; // SMTP username
+                                    $mail->Password = $itmpsswrd; // SMTP password
+                                    break;
+                                default:
+                                    $mail->Host = ('mail.'.$itmdomain);                                    
+                                    }
+                            $mail->From = $itmemail;
+                            $mail->FromName = $itmaccount;
+                            $mail->AddAddress($studentemail, $studentfullname);
+                            $mail->AddReplyTo($itmemail, $itmaccount);
                             $mail->WordWrap = 60;
                             $mail->IsHTML(true);
-                            $mail->Subject = $esubject;
-                            $mail->Body = $ebody;
-                            if(!$mail->Send())  {
+                            $mail->Subject = $post_subject;
+                            $mail->Body = $post_body;
+                            if(!$mail->Send()) {
                                 $feedback = ('Message could not be sent. Mailer Error: '.$mail->ErrorInfo);
                                 exit;
                             }else{
@@ -66,9 +108,10 @@
                        }
                     }
                 }
-                fclose($csv);
-                if (unlink('data/students.csv')){
+                fclose($openstudents);
+                if (unlink($datastudents)){
                     $feedback.=' Data removed.';
+                    $optionto = $optionNONE;
                 }else{
                     $feedback.=' Data still there?!?';
                 }
@@ -76,9 +119,10 @@
                 $feedback = ('Welcome! All fields are required.');
             }
         }
+        
     }else{
         $feedback = ('Awaiting mailing list data. Once data arrives, refresh this page.');
-        $optionstatus='<option value="">* AWAITING DATA *</option>';
+        $optionto = $optionNONE;
         $submit = $nosubmit;
     }
     
@@ -89,7 +133,10 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>ITM Mass Email</title>
         <link rel="stylesheet" type="text/css" href="styles/tinymce.css" />
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="styles/validationEngine.jquery.css" />
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+        <script type="text/javascript" charset="utf-8" src="js/languages/jquery.validationEngine-en.js"></script>
+        <script type="text/javascript" charset="utf-8" src="js/jquery.validationEngine.js"></script>
         <script type="text/javascript" src="tinymce/jscripts/tiny_mce/tiny_mce.js" ></script>
         <script type="text/javascript">
             tinyMCE.init({
@@ -108,35 +155,42 @@
 
                 });
     </script>
+        <script>
+            $(document).ready(function(){
+                $("#form.id").validationEngine();
+            });
+        </script>
     </head>
     <body>
         <div id="page">
             <h1>Welcome to ITM Mass Email</h1>
             <h2><?=$feedback; ?></h2>
-            <form method="post" action="" id="frmEmailCSV">
+            <form method="post" action="" id="frmMassEmail">
                 <div class="row">
-                    <label id="lblaccountname" for="accountname">Your Name</label>
-                    <input name="accountname" type="text" size="40" title="Account Name is a required field" value="" />
-                </div>
-                <div class="row">
-                    <label id="lblaccountemail" for="accountemail">Your Email Address</label>
-                    <input name="accountemail" type="text" size="40" title="Account Email is a required field" value="" />
-                </div>
-                <div class="row">
-                    <label id="lblstudentstatus" for="studentstatus">Student Status</label>
-                    <select name="studentstatus" title="Student Status is a required field">
-                        <?=$optionstatus ?>
+                    <label id="lbloptionfrom" for="optionfrom">FROM (Select One):</label>
+                    <select id="optionfrom" name="optionfrom" title="FROM is a required field"
+                            class="validate[required]">
+                        <?=$optionfrom ?>
                     </select>
                 </div>
                 <div class="row">
-                    <label id="lblemailsubject" for="emailsubject">Email Subject</label>
-                    <input name="emailsubject" type="text" size="40" title="Email Subject is a required field" value="" />
+                    <label id="lbloptionto" for="optionto">TO (Select One):</label>
+                    <select id="optionto" name="optionto" title="TO is a required field"
+                            class="validate[required]">
+                        <?=$optionto ?>
+                    </select>
                 </div>
                 <div class="row">
-                    <label id="lblemailbody" for="emailbody">Email Body:</label>
+                    <label id="lblemailsubject" for="emailsubject">SUBJECT:</label>
+                    <input id="emailsubject" name="emailsubject" type="text" size="40" title="SUBJECT is a required field" value=""
+                            class="validate[required] text-input"/>
                 </div>
                 <div class="row">
-                    <textarea name="emailbody" title="Email Body is a required field"></textarea>
+                    <label id="lblemailbody" for="emailbody">MESSAGE:</label>
+                </div>
+                <div class="row">
+                    <textarea id="emailbody" name="emailbody" title="MESSAGE is a required field"
+                            class="validate[required] text-input"/></textarea>
                 </div>
                 <div class="rowbtn">
                     <?=$submit; ?>
