e700d7ad025d0e85b59551242733a3cf2d121614
Jeffrey Hartmann <jephmann@gmail.com>
2011-08-12 15:19:51 -0500

    1st commit: nav.php (include)

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="admin.php">Add E-Mail Account</a></li>
+        <li><a href="upload.php">Upload Mailing List</a></li>
+        <li><a href="send.php">Send a Mass E-Mail</a></li>
+        <li><a href="index.php?logout">Logout</a></li>
+    </ul>
+</nav>
