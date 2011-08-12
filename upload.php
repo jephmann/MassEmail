dfede34b58c2718509daaba85cb0edb08c5d8b3e
Jeffrey Hartmann <jephmann@gmail.com>
2011-08-12 15:02:27 -0500

    upload.php: checks for csv and other arguments

--- upload.php
+++ upload.php
@@ -2,62 +2,58 @@
     include('config/session.php');
     include('config/db.php');
     
-    // RETRIEVE LAST UPDATED TIMESTAMP FROM TABLES
-    /*
-    function lastupdated($host,$un,$pw,$db,$table){
-        $connection=mysql_connect($host,$un,$pw) or die ('Unable to connect!');
-        mysql_select_db($db,$connection);
-        // $dataArray = mysql_fetch_array(mysql_query("show table status from ".$db." like ".$table));
-        // $lastupdated = $dataArray['Update_time']; 
-        mysql_close($connection);
-        return $db,$table;
-        
-        }
-    */
-    
-    if (isset($_POST['uploadStudents']) && $_FILES['csvstudents']['size'] > 0) {
-        
-        $fileName = $_FILES['csvstudents']['name'];
-        $tmpName  = $_FILES['csvstudents']['tmp_name'];        $
-        $fileSize = $_FILES['csvstudents']['size'];
-        $fileType = $_FILES['csvstudents']['type'];
-        $ext = strtolower(end(explode('.', $_FILES['csvstudents']['name'])));
-        // test stuff        
-        //echo '<br/>Filename: '.$fileName;        
-        //echo '<br/>Tempname: '.$tmpName;        
-        //echo '<br/>Filesize: '.$fileSize;        
-        //echo '<br/>Filetype: '.$fileType;
-        //echo '<br/>Extension '.$ext;
-        
-        // truncate students table
-        $connection=mysql_connect($host,$un,$pw) or die ('Unable to connect!');
-        mysql_select_db($db,$connection);
-        $query=("TRUNCATE TABLE `students`");
-        mysql_query($query) or die('<p>Error, TRUNCATE query failed</p>');
-        mysql_close($connection);
-        // upload to MySQL
-        $openstudents = fopen($tmpName, 'r');
-        while (!feof($openstudents)) {
-            $studentrecord = fgetcsv($openstudents, 1024);
-            $studentemail=$studentrecord[0];
-            $studentfullname=$studentrecord[1];
-            $studentstatus=$studentrecord[2];
-            $studentfullname=addslashes($studentfullname);
-            if ($studentemail != null || $studentemail != '' || strlen(trim($studentemail)) != 0){
-                $connection=mysql_connect($host,$un,$pw) or die ('Unable to connect!');
-                mysql_select_db($db,$connection);
-                $query=("INSERT INTO students (email, name, status) VALUES ('$studentemail','$studentfullname','$studentstatus')");
-                // echo '<p>'.$studentfullname.'</p>';
-                mysql_query($query) or die('<p>Error, INSERT query failed</p>');
-                mysql_close($connection);
+    if (isset($_POST['uploadCSV']) && $_FILES['csv']['size'] > 0) {        
+        if($_FILES['csv']['error'] == 0){        
+            $fileName = $_FILES['csv']['name'];
+            $ext = strtolower(end(explode('.', $fileName)));        
+            if($ext==='csv'){
+                $tmpName  = $_FILES['csv']['tmp_name'];
+                $fileSize = $_FILES['csv']['size'];
+                $fileType = $_FILES['csv']['type'];
+                // test stuff        
+                //echo '<br/>Filename: '.$fileName;        
+                //echo '<br/>Tempname: '.$tmpName;        
+                //echo '<br/>Filesize: '.$fileSize;        
+                //echo '<br/>Filetype: '.$fileType;
+                //echo '<br/>Extension '.$ext; 
+                if(($openCSV=fopen($tmpName, 'r')) !== false);{
+                    set_time_limit(0);
+                    // truncate (delete) old table data
+                    $connection=mysql_connect($host,$un,$pw) or die ('Unable to connect!');
+                    mysql_select_db($db,$connection);
+                    $query=("TRUNCATE TABLE `students`");
+                    mysql_query($query) or die('<p>Error, TRUNCATE query failed</p>');
+                    mysql_close($connection);
+                    // upload (insert) new data
+                    while (!feof($openCSV)) {
+                        $csvdata = fgetcsv($openCSV, 1024);
+                        // "sanitize"
+                        $studentemail=trim($csvdata[0]);
+                        $studentfullname=trim($csvdata[1]);
+                        $studentstatus=trim($csvdata[2]);
+                        $studentfullname=addslashes($studentfullname);
+                        // if csv record includes an email address, upload to database
+                        // (might be more compact to check against regular expression)
+                        if ($studentemail != null || $studentemail != '' || strlen(trim($studentemail)) != 0){
+                            $connection=mysql_connect($host,$un,$pw) or die ('Unable to connect!');
+                            mysql_select_db($db,$connection);
+                            $query=("INSERT INTO students (email, name, status) VALUES ('$studentemail','$studentfullname','$studentstatus')");
+                            // echo '<p>'.$studentfullname.'</p>';
+                            mysql_query($query) or die('<p>Error, INSERT query failed</p>');
+                            mysql_close($connection);
+                        }
+                    }
+                    fclose($openCSV);
+                    echo ('<script>alert("Student Mailing List UPDATED!")</script>');
+                }
+
+            }else{
+                echo ('<script>alert("File must be CSV.")</script>');
             }
-        }
-        echo ('<script>alert("Student Mailing List UPDATED!")</script>');
+        }else{
+            echo ('File Error ('.$_FILES['csv']['error'].')');
+        }        
     }
-    
-    //if (isset($_POST['uploadStudents'])) {
-        
-    //}
 ?>
 <!DOCTYPE html>
 <html>
@@ -73,27 +69,35 @@
         <script type="text/javascript" charset="utf-8" src="js/jquery.validationEngine.js"></script>
         <script>
             $(document).ready(function(){
-                $("#frmUpload").validationEngine();
+                $("#frmUploadCSV").validationEngine();
             });
         </script>
     </head>
     <body>
         <div id="page">
-    <?php include('inc/header.php'); ?>
-    <?php include('inc/nav.php'); ?>
-            <h2>Upload Student Mailing List</h2>
-            <p>Doublecheck the following before uploading:</p>
-            <ul>
-                <li>Are you uploading a csv file?</li>
-                <li>Make sure that the csv file has no column headers or field names.</li>
-                <li>Make sure that the csv file presents the following data in the following order:<br />
-                    Student email address, Student name, Student status</li>
-            </ul>
-            <form id="frmUpload" name="frmUpload" method="post" enctype="multipart/form-data" action="">
-                <input type="file" name="csvstudents" id="csvstudents"
-                           class="validate[required] text-input"/>
-                <input type="submit" class="btnsubmit" value="Upload Student File" name="uploadStudents" id="uploadStudents"/>
-            </form>
+            <?php include('inc/header.php'); ?>
+            <?php include('inc/nav.php'); ?>
+            <div id="content">
+                <h2>Upload Student Mailing List</h2>
+                <p>Doublecheck the following before uploading:</p>
+                <ul class="ulistdisc">
+                    <li>Are you uploading a CSV file?</li>
+                    <li>The CSV file must have no column headers or field names.</li>
+                    <li>The CSV file must present the following data in the following sequence:<br />
+                        <span class="code">Student email address, Student name, Student status</span></li>
+                </ul>
+                <div class="form">
+                    <br/>
+                    <form id="frmUploadCSV" name="frmUploadCSV" method="post" enctype="multipart/form-data" action="">
+                        <div class="row">
+                            <input type="file" name="csv" id="csv" size="100" class="validate[required] text-input"/>
+                        </div>
+                        <div class="rowbtn">
+                            <input type="submit" class="btnsubmit" value="Upload Mailing List" name="uploadCSV" id="uploadCSV"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </body>
 </html>
