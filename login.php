f216ef5c13950bd3a942055704aa27276407179b
Jeffrey Hartmann <jephmann@gmail.com>
2011-08-14 18:37:27 -0500

    1st commit: login.php; jquery login script now in a js file

--- login.php
+++ login.php
@@ -9,51 +9,7 @@
         <link rel="stylesheet" type="text/css" href="styles/reset.css" />
         <link rel="stylesheet" type="text/css" href="styles/common.css" />
         <script src="js/jquery.js" type="text/javascript" language="javascript"></script>
-        <script language="javascript">
-          //  Developed by Roshan Bhattarai
-          //  Visit http://roshanbh.com.np for this script and more.
-          //  This notice MUST stay intact for legal use
-
-        $(document).ready(function()
-        {
-            $("#login_form").submit(function()
-            {
-                //remove all the class add the messagebox classes and start fading
-                $("#msgbox").removeClass().addClass('messagebox').text('Validating....').fadeIn(1000);
-                //check the username exists or not from ajax
-                $.post("ajax_login.php",{ user_name:$('#username').val(),password:$('#password').val(),rand:Math.random() } ,function(data)
-                {
-                    if(data=='yes') //if correct login detail
-                    {
-                        $("#msgbox").fadeTo(200,0.1,function()  //start fading the messagebox
-                        {
-                            //add message and change the class of the box and start fading
-                            $(this).html('Excuse me while I whip this out.....').addClass('messageboxok').fadeTo(900,1,
-                            function()
-                            {
-                                //redirect to secure page
-                                document.location='index.php';
-                            });
-                        });
-                    }
-                    else
-                    {
-                        $("#msgbox").fadeTo(200,0.1,function() //start fading the messagebox
-                        {
-                            //add message and change the class of the box and start fading
-                            $(this).html('Mother puss bucket...').addClass('messageboxerror').fadeTo(900,1);
-                        });
-                    }
-                });
-                return false; //not to post the  form physically
-                });
-                //now call the ajax also focus move from
-                $("#password").blur(function()
-                {
-                    $("#login_form").trigger('submit');
-                });
-            });
-        </script>
+        <script src="js/jquery.login.roshanbh.js" type="text/javascript" language="javascript"></script>
         <link rel="stylesheet" type="text/css" href="styles/login.css" />
     </head>
     <body>
@@ -71,7 +27,7 @@
                             <input name="password" type="password" id="password" value="" maxlength="20" />
                         </div>
                         <div class="buttondiv">
-                            <input name="Submit" type="submit" id="submit" class="btnsubmit" value="Login" style="margin-left:-10px; height:23px"  />
+                            <input name="Submit" type="submit" id="submit" class="btnsubmit" value="Engage" style="margin-left:-10px; height:23px"  />
                             <span id="msgbox" style="display:none"></span>
                         </div>
                     </div>
